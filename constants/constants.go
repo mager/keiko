2d231f16e6c9109e7d4abfef31de02943e489ff8
Mager <andrew.mager@gmail.com>
2022-01-06 16:57:59 -0600

    Try to format stats for the chart

---
+++ constants/constants.go
@@ -0,0 +1,3 @@
+package constants
+
+var DefaultAddress = "0x0000000000000000000000000000000000000000"
--- handler/get_collection.go
+++ handler/get_collection.go
@@ -9,15 +9,20 @@ import (
 
 	"cloud.google.com/go/bigquery"
 	"github.com/gorilla/mux"
+	"github.com/mager/keiko/constants"
 	"github.com/mager/keiko/opensea"
 	"github.com/mager/keiko/utils"
 	"github.com/mager/sweeper/database"
 	"google.golang.org/api/iterator"
 )
 
-type Stat struct {
+type BQStat struct {
 	Floor     float64   `json:"floor"`
 	Timestamp time.Time `json:"timestamp"`
+}
+type Stat struct {
+	Date  string  `json:"date"`
+	Floor float64 `json:"floor"`
 }
 
 type GetCollectionResp struct {
@@ -41,7 +46,7 @@ func (h *Handler) getCollection(w http.ResponseWriter, r *http.Request) {
 		users       = h.database.Collection("users")
 		address     = r.Header.Get("X-Address")
 		slug        = mux.Vars(r)["slug"]
-		stats       = make([]Stat, 0)
+		stats       = make([]BQStat, 0)
 	)
 
 	// Fetch collection from database
@@ -75,7 +80,7 @@ func (h *Handler) getCollection(w http.ResponseWriter, r *http.Request) {
 		SELECT Floor, RequestTime, SevenDayVolume
 		FROM `+"`floor-report-327113.collections.update`"+`
 		WHERE slug = "%s"
-		ORDER BY RequestTime DESC
+		ORDER BY RequestTime
 	`, slug))
 	it, _ := q.Read(ctx)
 	for {
@@ -87,16 +92,17 @@ func (h *Handler) getCollection(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			return
 		}
-		stats = append(stats, Stat{
+		stats = append(stats, BQStat{
 			Floor:     values[0].(float64),
 			Timestamp: values[1].(time.Time),
 		})
 	}
 
-	resp.Stats = stats
+	// resp.Stats = h.adaptStats(stats)
+	resp.Stats = h.adaptStats(stats)
 
 	// Check if the user is following the collection
-	if address != "" {
+	if address != "" || address != constants.DefaultAddress {
 		var db database.User
 		docsnap, err := users.Doc(address).Get(ctx)
 		if err != nil {
@@ -114,4 +120,50 @@ func (h *Handler) getCollection(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(resp)
+}
+
+func (h *Handler) adaptStats(stats []BQStat) []Stat {
+	var statsByDate = h.reduceStats(stats)
+
+	var result = make([]Stat, 0)
+
+	for _, stat := range statsByDate {
+		result = append(result, Stat{
+			Date:  h.formatStatDate(stat),
+			Floor: stat.Floor,
+		})
+	}
+
+	return result
+}
+
+// Only return one stat per day, filter out 0 values
+func (h *Handler) reduceStats(stats []BQStat) []BQStat {
+	var reducedByDate = []BQStat{}
+	var reducedRemoveZeros = []BQStat{}
+
+	// Return one stat per day
+	for _, stat := range stats {
+		if len(reducedByDate) == 0 {
+			reducedByDate = append(reducedByDate, stat)
+		} else {
+			h.logger.Info(stat)
+			if stat.Timestamp.Day() != reducedByDate[len(reducedByDate)-1].Timestamp.Day() {
+				reducedByDate = append(reducedByDate, stat)
+			}
+		}
+	}
+
+	// Remove 0 values
+	for _, stat := range reducedByDate {
+		if stat.Floor != 0 {
+			reducedRemoveZeros = append(reducedRemoveZeros, stat)
+		}
+	}
+
+	return reducedRemoveZeros
+}
+
+func (h *Handler) formatStatDate(stat BQStat) string {
+	return stat.Timestamp.Format("01-02")
 }
